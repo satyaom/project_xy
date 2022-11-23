34837994e31dd4523d0ccc5a3391da4aecbf8965
satyaom <satyaom1998@gmail.com>
2022-11-23 21:36:40 +0530

    fix controller dir name

--- project_xy_backend/src/main/java/com/example/project_xy_backend/controller/HomeController.java
+++ project_xy_backend/src/main/java/com/example/project_xy_backend/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.example.project_xy_backend.Controller;
+package com.example.project_xy_backend.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
