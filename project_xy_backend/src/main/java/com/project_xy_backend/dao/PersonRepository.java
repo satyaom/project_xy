003ce7db04e8cd62664cc48d3270134858b15428
satyaom <satyaom1998@gmail.com>
2022-12-20 00:16:38 +0530

    Added updatePerson query

--- project_xy_backend/src/main/java/com/project_xy_backend/dao/PersonRepository.java
+++ project_xy_backend/src/main/java/com/project_xy_backend/dao/PersonRepository.java
@@ -3,11 +3,17 @@ package com.project_xy_backend.dao;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.project_xy_backend.model.Person;
 
 public interface PersonRepository extends JpaRepository<Person, Long>{
   @Query("SELECT s FROM Person s WHERE s.email = ?1")
   Optional<Person> findPersonByEmail(String email);
+
+  @Modifying
+  @Query("UPDATE Person s SET s.name = :newPersonName WHERE s.id = :id")
+  void updatedPeson(@Param("id") Long id, @Param("newPersonName") String newPersonName);
 }
