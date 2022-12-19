51b7917cf58dfd949ee09313f3bb687b97201021
satyaom <satyaom1998@gmail.com>
2022-12-20 00:15:05 +0530

    Added delete and patch mapping

--- project_xy_backend/src/main/java/com/project_xy_backend/controller/PersonController.java
+++ project_xy_backend/src/main/java/com/project_xy_backend/controller/PersonController.java
@@ -4,7 +4,10 @@ import java.util.List;
 
 import javax.validation.Valid;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,5 +34,15 @@ public class PersonController {
   @PostMapping
   public void addNewPerson(@RequestBody @Valid Person person) {
     personService.addNewPerson(person);
+  }
+
+  @DeleteMapping(path = "{id}")
+  public void deletePerson(@PathVariable("id") Long id) {
+    personService.deletePerson(id);
+  }
+
+  @PatchMapping(path = "{id}")
+  public void updatePerson(@PathVariable("id") Long id, @RequestBody @Valid Person newPerson) {
+    personService.updatePerson(id, newPerson.getName());
   }
 }
