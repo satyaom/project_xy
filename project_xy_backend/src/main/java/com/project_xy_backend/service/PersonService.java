7b13e79f484dee450c5a25783efa9468e77722e9
satyaom <satyaom1998@gmail.com>
2022-12-20 00:15:37 +0530

    Delete and update person transaction

--- project_xy_backend/src/main/java/com/project_xy_backend/service/PersonService.java
+++ project_xy_backend/src/main/java/com/project_xy_backend/service/PersonService.java
@@ -1,6 +1,9 @@
 package com.project_xy_backend.service;
 
 import java.util.List;
+import java.util.Optional;
+
+import javax.transaction.Transactional;
 
 import org.springframework.stereotype.Service;
 
@@ -25,6 +28,26 @@ public class PersonService{
       throw new IllegalStateException("email taken");
     }
     personRepository.save(person);
+  }
+
+  @Transactional
+  public void deletePerson(Long id) {
+    if(personRepository.findById(id).isPresent()) {
+      personRepository.deleteById(id);
+    } else {
+      throw new IllegalAccessError("wrong id");
+    }
+  }
+
+  @Transactional
+  public void updatePerson(Long id, String newPersonName) {
+    Optional<Person> oldPerson = personRepository.findById(id);
+    if(oldPerson.isPresent()){
+      System.out.println(newPersonName);
+      personRepository.updatedPeson(id, newPersonName);
+     } else {
+      throw new IllegalAccessError("wrong id");
+     }
   }
 
 }
